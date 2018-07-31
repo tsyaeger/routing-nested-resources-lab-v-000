799ddce41b73abc0281f0e00e35d0cd5a7cb8cc4
taryn yaeger <tsyaeger@gmail.com>
2018-07-31 03:19:52 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  resources :artists
-  resources :songs
+  
+  resources :artists, only: [:show] do 
+    resources :songs, only: [:show, :index]
+  end
+    
+  resources :songs, only: [:index, :show, :new, :create, :edit, :update]  
+  
+  root 'songs#index'
+    
 end
+
